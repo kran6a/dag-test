3159f3ee0609ddb08ef910f58e7f030ca26101d1
kran6a <kran6a@gmail.com>
2022-07-23 17:21:07 +0200

    Remove unneeded dependencies

--- src/classes/Account.ts
+++ src/classes/Account.ts
@@ -22,17 +22,17 @@ export default class Account {
     static from_binary(bin: Uint8Array): [Account, number] {
         const ret: Account = new Account([]);
         if (bin.length < 2+PUBKEY_BYTE_LENGTH)
-            throw new Error("Bad binary payload");
+            throw new Error("Bad binary payload", {cause: new Error("Binary length is shorter than the minimal amount of bytes needed to store a single account")});
         if (bin[0] !== TRANSITION_TYPES.ACCOUNT)
-            throw new Error("Bad binary payload");
+            throw new Error("Bad binary payload", {cause: new Error('Attempted to instantiate an Account transition with an invalid transition byte')});
         const pubkey_count: number = bin[1] + 1;
         let offset: number = 2;
         if (bin.length < offset+pubkey_count*PUBKEY_BYTE_LENGTH) //Payload without metadata must be a multiple of the pubkey length
-            throw new Error("Bad binary payload");
+            throw new Error("Bad binary payload", {cause: new Error("The length of a payload stripped out of metadata must be a multiple of the length of a single pubkey")});
         for (let i=0;i<pubkey_count;i++) {
             const pubkey: string = buffer2string(bin.slice(offset, offset+=PUBKEY_BYTE_LENGTH), 'hex');
             if (ret.pubkeys.includes(pubkey)) //Pubkeys must be unique
-                throw new Error("Bad binary payload");
+                throw new Error("Bad binary payload", {cause: new Error("Duplicated pubkeys")});
             ret.add(pubkey);
         }
         ret.binary = ()=>bin.slice(0, offset);
